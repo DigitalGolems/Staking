36aa5d62a823eeeb6d9b4e5a64705cd9f0b235f1
DigitalGolems <97181362+DigitalGolems@users.noreply.github.com>
2022-02-01 12:32:43 +0300

    First commit
    Staking DBT

---
+++ contracts/StakingDBT.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma experimental ABIEncoderV2;
+pragma solidity ^0.8.10;
+
+contract StakingDBT {
+    
+    function blockTokens() external {
+
+    }
+
+    function farmDIG() external {
+
+    }
+
+    function _sendDIG() internal {
+        
+    }
+
+}
